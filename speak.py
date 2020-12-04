4530bc1b15018b88b0c8cb5e3a668adb18c52003
Yoplayer-1 <66262055+Yoplayer-1@users.noreply.github.com>
2020-12-04 09:09:35 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -1,15 +1,15 @@
 """
-This module uses pyttsx3 to speak as the first step in every pyttsx3 app is this so I am making a module for it.
+This module uses pyttsx3 to speak as the first step in every pyttsx3 script is this so I am making a module for it.
 """
+Class Speak:
+    import pyttsx3
 
-import pyttsx3
+    self.engine = pyttsx3.init()
 
-engine = pyttsx3.init()
+    def speak(self,audio):
+        self.engine.say(audio)
+        self.engine.runAndWait()
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-  
-  
- # Hopefully this will run
+
+# Hopefully this will run
 speak("Hello user thanks for downloading this module")
