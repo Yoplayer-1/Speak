3f1260a5c691175937928ef959dc61b5110cf050
Yoplayer-1 <66262055+Yoplayer-1@users.noreply.github.com>
2020-12-04 09:11:54 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -1,12 +1,13 @@
-"""
-This module uses pyttsx3 to speak as the first step in every pyttsx3 script is this so I am making a module for it.
-"""
+""" This module uses pyttsx3 to speak as the first step in every pyttsx3 script is this so I am making a module for it."""
+
 Class Speak:
+    """ I am bad in classes but since pypi requries one I'll give it a go """
     import pyttsx3
 
-    self.engine = pyttsx3.init()
+    self.engine = pyttsx3.init() # This will initialize pyttsx3
 
     def speak(self,audio):
+        """ The main Function"""
         self.engine.say(audio)
         self.engine.runAndWait()
 
