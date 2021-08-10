f803428cd7bb3f9395ff24a6d33928eef79459d8
Yoplayer-1 <66262055+Yoplayer-1@users.noreply.github.com>
2021-08-10 11:13:38 +0530

    Update tests.py
    A test for test...

--- tests.py
+++ tests.py
@@ -1,1 +1,13 @@
 
+from speak import Speak
+
+client = Speak()
+client.speak("Hello") # Remove the Hello string with any other string ##
+
+#eg:
+def input_speak(name):
+  client.speak(f"Hi {name}")
+ 
+#use the function like:
+name1 = input("What's your name?: ")
+input_speak(name1)
